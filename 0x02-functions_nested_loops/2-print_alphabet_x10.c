b3d2de69865a09873541868049bb0bfbfcfb2528
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-15 14:07:00 -0700

    alphabet 10X

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet in lower-case
+ * print_alphabet - print 10 * alphabet in lower-case
  *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 
 	int i;
