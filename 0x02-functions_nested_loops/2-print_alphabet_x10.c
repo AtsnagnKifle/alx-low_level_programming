df3869a0831091122a858a2dad25221076516747
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-15 14:05:12 -0700

    alphabet 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lower-case
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+	int i;
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (i = 97 ; i < 123 ; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
