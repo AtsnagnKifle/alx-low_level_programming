0624f9026407dd801273b27d310eb5812d0b4603
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-15 16:22:44 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - print sum of all the multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sum;
+	int i;
+
+	for (i = 1 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i & 5 == 0)
+			sum = sum + i;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,34 @@
 void print_to_98(int n)
 {
 	int i;
-	/*int no;
-	int ar[10] = {48,49,50,51,52,53,54,55,56,57};*/
+	int no;
+	int asc[10] = {48,49,50,51,52,53,54,55,56,57};
+	int arr[20];
 
 	if (n>98)
 	{
 		for ( i = n ; i > 98 ; i--)
 		{
-			_putchar((char)i);
-			_putchar(',');
-			_putchar(' ');
+			no = i;
+			arr = {};
+			int co = 0;
+			int last_digit;
+
+			while ( no != 0)
+			{
+				last_digit = no % 10;
+				no = no / 10;
+				arr[co] = last_digit;
+				co++;
+			}
+			co --;
+
+			while ( co > -1)
+			{
+				_putchar(asc[arr[co]]);
+				_putchar( ',' );
+				_putchar( ' ' );
+			}
 		}
 	}
 	else
