31dc1681cea81af6b61c0e5d20cd651147c8f02f
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-15 17:30:21 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,46 +10,20 @@
 void print_to_98(int n)
 {
 	int i;
-	int no;
-	int asc[10] = {48,49,50,51,52,53,54,55,56,57};
-	int arr[20];
 
-	if (n>98)
+	if (n > 98)
 	{
-		for ( i = n ; i > 98 ; i--)
+		for (i = n ; i > 98 ; i--)
 		{
-			no = i;
-			arr = {};
-			int co = 0;
-			int last_digit;
-
-			while ( no != 0)
-			{
-				last_digit = no % 10;
-				no = no / 10;
-				arr[co] = last_digit;
-				co++;
-			}
-			co --;
-
-			while ( co > -1)
-			{
-				_putchar(asc[arr[co]]);
-				_putchar( ',' );
-				_putchar( ' ' );
-			}
+			printf("%d, ", i);
 		}
-	}
 	else
 	{
-		for ( i = n ; i < 98 ; i++)
+		for (i = n ; i < 98 ; i++)
 		{
-			_putchar(i);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", i);
 		}
 	}
 
-	_putchar('9');
-	_putchar('8');
+	printf("98, ");
 }
