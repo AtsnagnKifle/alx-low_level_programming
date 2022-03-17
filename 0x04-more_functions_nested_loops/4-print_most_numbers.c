47284150cc5ed13d371d9aaf0085fcc240621b37
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-17 04:10:04 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ */
+
+void print_numbers()
+{
+	int c;
+
+	if (c >= 48 && c <= 57)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
