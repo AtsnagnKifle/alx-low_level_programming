5e14280bf068f843f213030152da2db5bf8186cc
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-21 11:59:06 -0700

    str len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include<string.h>
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _strlen - returns length of a str
+ *
+ *Return: length of str
+ *@s: parameter s
+ */
+
+int _strlen(char *s)
+{
+
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+
+}
