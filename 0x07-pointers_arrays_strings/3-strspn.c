adf01580c5df16d2de999776930b9a2556a1ac29
atsnagnkifle <kifleatsnagn@gmail.com>
2022-03-28 05:15:47 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stddef.h>
+#include<string.h>
+
+/**
+ * *_strspn - strspn
+ *
+ *Return: int strspn
+ *@s:char array (String)
+ *@accept:char array (String)
+ */
+
+unsigned int _strspn(char *s, char *accept);
+{
+	return (strspn(s, accept));
+}
