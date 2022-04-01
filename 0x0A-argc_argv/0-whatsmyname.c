84c009dfd4488da15f12b64c5678a78c1fd7f4d9
atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-01 15:29:33 -0700

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+
+/**
+ * main - print name
+ *
+ *@argc: counter
+ *@argv: array
+ *Return: Always return  0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
