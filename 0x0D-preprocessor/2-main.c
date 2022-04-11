994343513647ec567980187195a1eb0059afeb7a
atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-11 15:07:55 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - print the file name it was compiled from
+ *
+ * ReturnL Always return success (0)
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
