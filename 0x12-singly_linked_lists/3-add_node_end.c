9b759e99f621cacb232b8bbee6b96f2693792f26
atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-22 15:38:14 -0700

    betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,6 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *newNode;
 
 	newNode = (list_t *)malloc(sizeof(list_t));
+
 	if (newNode == NULL)
 		return (NULL);
 	newNode->str = strdup(str);
@@ -25,6 +26,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		list_t *lastNode = *head;
+
 		while (lastNode->next != NULL)
 		{
 			lastNode = lastNode->next;
