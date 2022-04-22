7e6424610f60e6c351fda7b44491c290ce9031e4
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-23 01:36:07 +0300

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,19 +19,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
 	newNode->next = NULL;
-	/* check if list is null and make new node the head */
+
 	if (*head == NULL)
 		*head = newNode;
-	/* else find last node and add new node */
 	else
 	{
 		list_t *lastNode = *head;
-		/* last node has to be NULL use while to check */
 		while (lastNode->next != NULL)
 		{
 			lastNode = lastNode->next;
 		}
-		/* if null is found add new node at the end */
 		lastNode->next = newNode;
 	}
 	return (newNode);
